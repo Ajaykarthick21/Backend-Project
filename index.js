14ccf4d69fdd500c3ef0ce1dd50643cc793b9770
Ajaykarthick21 <ajaykarthick1998@gmail.com>
2024-06-02 14:34:37 +0530

    New changes

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const PORT = 4000;
-require('dotenv').config
+import 'dotenv/config'
 
+const port = process.env.PORT || 3000
+app.get('/api/jokes',(req,res)=>{
+const jokes = [
+  {
+    id: 1,
+    title: "Why don't scientists trust atoms?",
+    joke: "Because they make up everything!"
+  },
+  {
+    id: 2,
+    title: "How do you organize a space party?",
+    joke: "You planet."
+  },
+  {
+    id: 3,
+    title: "Why did the scarecrow win an award?",
+    joke: "Because he was outstanding in his field!"
+  },
+  {
+    id: 4,
+    title: "Why don't programmers like nature?",
+    joke: "It has too many bugs."
+  },
+  {
+    id: 5,
+    title: "What do you get when you cross a snowman and a vampire?",
+    joke: "Frostbite."
+  }
+];
+
+    res.send(jokes)
+
+})
 
 app.get('/',(req,res)=>{
-    res.send("Hello world")
-    
-
+  res.send('<h1>Welcome to the world of mark antony mame</h1>')
 })
 
-app.get('/twitter',(req,res)=>{
-    res.send("<h1>Kya rey setting ah</h1>")
-})
-app.listen(process.env.PORT,(req,res)=>{
-    console.log(`Server is running in ${PORT}`)
+app.listen(port,(req,res)=>{
+    console.log(`Server is running in ${port}`)
 })
